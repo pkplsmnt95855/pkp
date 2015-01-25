6e293a656eb8a02656334c30a4700e67c2b6def3
test <test@test.com>
2015-01-25 22:00:12 +0500

    updates

--- protected/config/main.php
+++ protected/config/main.php
@@ -5,6 +5,7 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
+
     'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
     'name' => 'My Web Application',
     // preloading 'log' component
